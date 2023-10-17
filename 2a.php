d5bc6246361644b100477d3342ee82e9b51bf15a
pahmi <100520155+PahmiNugrh@users.noreply.github.com>
2023-10-17 08:46:13 +0700

    Update 2a.php

--- 2a.php
+++ 2a.php
@@ -4,7 +4,7 @@ transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"> 
     <head> 
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Hitung Penjualan Barang</title> 
+        <title>Perhitung Penjualan Barang</title> 
     </head> 
     <body> 
         <div align="center"><br />PERHITUNGAN PENJUALAN BARANG 2a.php</div> <br/><br/>
